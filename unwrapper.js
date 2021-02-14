1baa7315adc483214eda9c5aa659b9997542ec8c
Alexis <33222808+sathonay@users.noreply.github.com>
2021-02-14 10:30:21 +0100

    Update unwrapper.js

--- unwrapper.js
+++ unwrapper.js
@@ -1,8 +1,15 @@
+async function unwrapInner(wrapper) {
+    const toUnwrap = wrapper.getAttribute('unwrap')
+    const response = await fetch(toUnwrap)
+    wrapper.innerHTML = await response.text()
+}
 
-async function unwrap(wrapper) {
-    const toUnwrap = wrapper.getAttribute('src')
+async function unwrapOuter(wrapper) {
+    const toUnwrap = wrapper.getAttribute('unwrap')
     const response = await fetch(toUnwrap)
     wrapper.outerHTML = await response.text()
 }
 
-document.querySelectorAll('.js-unwrap').forEach(unwrap)
+document.querySelectorAll('.js-unwrap').forEach(unwrapInner)
+document.querySelectorAll('.js-unwrap-inner').forEach(unwrapInner)
+document.querySelectorAll('.js-unwrap-outer').forEach(unwrapOuter)
